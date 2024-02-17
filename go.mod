425791c5bac99f54bcaa08177b5b62cf931b7d6d
William H. Gilmore <whgi@gms-online.com>
2024-02-17 15:58:00 -0600

    Added Jet scenarios

--- go.mod
+++ go.mod
@@ -4,14 +4,20 @@ go 1.21.6
 
 require (
 	github.com/go-faker/faker/v4 v4.3.0
+	github.com/go-jet/jet/v2 v2.10.1
 	github.com/mattn/go-sqlite3 v1.14.22
 	github.com/schollz/progressbar/v3 v3.14.1
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/term v0.17.0 // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -12,14 +12,13 @@ import (
 	//"strings"
 
 	"github.com/go-faker/faker/v4"
-	//. "github.com/go-jet/jet/v2/sqlite"
+	. "github.com/go-jet/jet/v2/sqlite"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/schollz/progressbar/v3"
 
-	//. "go.local/wl-go/internal/gen/table"
+	"github.com/lbe/go-sql-test/gen/model"
+	. "github.com/lbe/go-sql-test/gen/table"
 	"github.com/lbe/go-sql-test/models"
-	//"go.local/wl-go/internal/gen/view"
-	//"go.local/wl-go/internal/gen/model"
 )
 
 type opts struct {
@@ -27,7 +26,9 @@ type opts struct {
 	flipYearBirth  *bool
 	rowCount       *int
 	updateCount    *int
+	useBoth        *bool
 	useJet         *bool
+	useRawSQL      *bool
 	useTransaction *bool
 }
 
@@ -156,7 +157,7 @@ func dbInit() (err error) {
 	return
 }
 
-func genData() (fakeData []models.RawSqlUser, err error) {
+func genData() (fakeData []model.User, err error) {
 	for i := 0; i < *opt.rowCount; i++ {
 		a := structFakeData{}
 		err := faker.FakeData(&a)
@@ -165,7 +166,7 @@ func genData() (fakeData []models.RawSqlUser, err error) {
 			log.Fatalf("[error] %s:%d %v", filename, line, err)
 		}
 		im := `@` + a.User
-		s := models.RawSqlUser{
+		s := model.User{
 			User:      a.User,
 			City:      &a.Address.City,
 			Region:    &a.Address.State,
@@ -181,7 +182,7 @@ func genData() (fakeData []models.RawSqlUser, err error) {
 	return
 }
 
-func insertWithRawSQLUpsert(data []models.RawSqlUser) {
+func insertWithRawSQLUpsert(data []model.User) {
 	log.Println("Executing insertWithRawSQLUpsert")
 
 	var tx *sql.Tx
@@ -230,7 +231,7 @@ func insertWithRawSQLUpsert(data []models.RawSqlUser) {
 	}
 }
 
-func updateWithRawSQLUpsert(data []models.RawSqlUser) {
+func updateWithRawSQLUpsert(data []model.User) {
 	if *opt.updateCount == 0 {
 		return
 	}
@@ -288,7 +289,7 @@ func updateWithRawSQLUpsert(data []models.RawSqlUser) {
 	}
 }
 
-func selectWithRawSQLUpsert(data []models.RawSqlUser) {
+func selectWithRawSQLUpsert(data []model.User) {
 	log.Println("Executing selectWithRawSQLUpsert")
 
 	var tx *sql.Tx
@@ -346,148 +347,239 @@ func selectWithRawSQLUpsert(data []models.RawSqlUser) {
 	}
 }
 
-/*
-	func withJet(data ExcelData, opt opts) {
-		log.Println("Executing withJet")
-		cur_time := time.Now()
-		cur_year := cur_time.Year()
+func insertWithJet(data []model.User) {
+	log.Println("Executing insertWithJet")
 
-		var tx *sql.Tx
+	var tx *sql.Tx
+	if *opt.useTransaction {
+		// Get a Tx for making transaction requests.
+		var err error
+		tx, err = opt.db.Begin()
+		if err != nil {
+			_, filename, line, _ := runtime.Caller(1)
+			log.Fatalf("[error] %s:%d %v", filename, line, err)
+		}
+		// Defer a rollback in case anything fails.
+		defer tx.Rollback()
+	}
+
+	bar := progressbar.Default(int64(len(data)))
+	for _, rec := range data {
+
+		columnList := ColumnList{
+			User.User, User.City, User.Region, User.Country, User.AreaCode, User.ZipCode,
+			User.YearBirth, User.Im, User.Name,
+		}
+
+		stmtInserUser := User.INSERT(columnList).
+			MODEL(rec).
+			ON_CONFLICT(User.User).
+			DO_UPDATE(
+				SET(
+					User.City.SET(User.EXCLUDED.City),
+					User.Country.SET(User.EXCLUDED.Country),
+					User.AreaCode.SET(User.EXCLUDED.AreaCode),
+					User.ZipCode.SET(User.EXCLUDED.ZipCode),
+					User.YearBirth.SET(User.EXCLUDED.YearBirth),
+					User.Im.SET(User.EXCLUDED.Im),
+					User.Name.SET(User.EXCLUDED.Name),
+				).WHERE(
+					OR(User.Country.IS_DISTINCT_FROM(User.EXCLUDED.Country)).
+						OR(User.AreaCode.IS_DISTINCT_FROM(User.EXCLUDED.AreaCode)).
+						OR(User.ZipCode.IS_DISTINCT_FROM(User.EXCLUDED.ZipCode)).
+						OR(User.YearBirth.IS_DISTINCT_FROM(User.EXCLUDED.YearBirth)).
+						OR(User.Im.IS_DISTINCT_FROM(User.EXCLUDED.Im)).
+						OR(User.Name.IS_DISTINCT_FROM(User.EXCLUDED.Name)),
+				),
+			)
+
+		// sql_debug := stmtInserUser.DebugSql()
+		// fmt.Println(sql_debug)
 		if *opt.useTransaction {
-			// Get a Tx for making transaction requests.
-			var err error
-			tx, err = *opt.db.Begin()
+			_, err := stmtInserUser.Exec(tx)
 			if err != nil {
 				_, filename, line, _ := runtime.Caller(1)
 				log.Fatalf("[error] %s:%d %v", filename, line, err)
 			}
-			// Defer a rollback in case anything fails.
-			defer tx.Rollback()
-		}
-
-		bar := progressbar.Default(int64(len(data.Rows)))
-		for _, row := range data.Rows {
-			var rec model.User
-			rec.User = row[data.Headers["user"]]
-			rec.City = getPtrNullableString(row[data.Headers["city"]])
-			rec.Region = getPtrNullableString(row[data.Headers["state"]])
-			rec.Country = getPtrNullableString(row[data.Headers["country"]])
-			rec.AreaCode = getPtrNullableString(row[data.Headers["area_code"]])
-			rec.ZipCode = getPtrNullableString(row[data.Headers["zip_code"]])
-			age_str := strings.TrimSpace(row[data.Headers["age"]])
-			age, err := strconv.Atoi(age_str)
-			if err == nil {
-				year_birth := cur_year - age
-				rec.YearBirth = getPtrNullableInt(int32(year_birth))
-			} else {
-				if *opt.flipYearBirth {
-					rec.YearBirth = getPtrNullableInt(int32(len(rec.User)))
-				} else {
-					rec.YearBirth = getPtrNullableInt(0)
-				}
+		} else {
+			_, err := stmtInserUser.Exec(opt.db)
+			if err != nil {
+				_, filename, line, _ := runtime.Caller(1)
+				log.Fatalf("[error] %s:%d %v", filename, line, err)
 			}
-			rec.Im = getPtrNullableString(row[data.Headers["im"]])
-			rec.NameFemale = getPtrNullableString(row[data.Headers["name"]])
-			rec.Target = getPtrNullableString(row[data.Headers["target"]])
-			rec.Shared = getPtrNullableString(row[data.Headers["shared"]])
-			rec.Active = getPtrNullableString(row[data.Headers["u"]])
-			// rec.CreatedTst = nil
-			// rec.ChangedTst = nil
-
-			//var year_birth sql.NullInt64
-			//if rec.year_birth == nil {
-			//	year_birth.Int64 = 0
-			//	year_birth.Valid = false
-			//} else {
-			//	year_birth.Int64 = int64(*rec.year_birth)
-			//	year_birth.Valid = true
-			//}
-			//fmt.Printf("%s %d\n", *rec.user, year_birth.Int64)
-			//upsertUser := models.Db.StmtUpsertUser()
-			//_, err = upsertUser().Exec(rec.user, rec.city, rec.region, rec.country, rec.area_code,
-			//	rec.zip_code, year_birth, rec.im, rec.name_female, rec.name_male,
-			//	rec.target, rec.shared, rec.active)
-
-			columnList := ColumnList{
-				User.User, User.City, User.Region, User.Country, User.AreaCode, User.ZipCode,
-				User.YearBirth, User.Im, User.NameFemale, User.NameMale, User.Target, User.Shared, User.Active,
-			}
-
-			//stmtUserExists := User.SELECT(
-			//	EXISTS(
-			//		SELECT(User.User),
-			//	),
-			//)
-
-			stmtInserUser := User.INSERT(columnList).
-				MODEL(rec).
-				ON_CONFLICT(User.User).
-				DO_UPDATE(
-					SET(
-						User.City.SET(User.EXCLUDED.City),
-						User.Country.SET(User.EXCLUDED.Country),
-						User.AreaCode.SET(User.EXCLUDED.AreaCode),
-						User.ZipCode.SET(User.EXCLUDED.ZipCode),
-						User.YearBirth.SET(User.EXCLUDED.YearBirth),
-						User.Im.SET(User.EXCLUDED.Im),
-						User.NameFemale.SET(User.EXCLUDED.NameFemale),
-						User.NameMale.SET(User.EXCLUDED.NameMale),
-						User.Target.SET(User.EXCLUDED.Target),
-						User.Shared.SET(User.EXCLUDED.Shared),
-						User.Active.SET(User.EXCLUDED.Active),
-					).WHERE(
-						OR(User.Country.IS_DISTINCT_FROM(User.EXCLUDED.Country)).
-							OR(User.AreaCode.IS_DISTINCT_FROM(User.EXCLUDED.AreaCode)).
-							OR(User.ZipCode.IS_DISTINCT_FROM(User.EXCLUDED.ZipCode)).
-							OR(User.YearBirth.IS_DISTINCT_FROM(User.EXCLUDED.YearBirth)).
-							OR(User.Im.IS_DISTINCT_FROM(User.EXCLUDED.Im)).
-							OR(User.NameFemale.IS_DISTINCT_FROM(User.EXCLUDED.NameFemale)).
-							OR(User.NameMale.IS_DISTINCT_FROM(User.EXCLUDED.NameMale)).
-							OR(User.Target.IS_DISTINCT_FROM(User.EXCLUDED.Target)).
-							OR(User.Shared.IS_DISTINCT_FROM(User.EXCLUDED.Shared)).
-							OR(User.Active.IS_DISTINCT_FROM(User.EXCLUDED.Active)),
-					),
-				)
-
-			// sql_debug := stmtInserUser.DebugSql()
-			// fmt.Println(sql_debug)
-			if *opt.useTransaction {
-				_, err = stmtInserUser.Exec(tx)
-				if err != nil {
-					fmt.Println(err, row)
-					// fmt.Println(1)
-				}
-			} else {
-				_, err = stmtInserUser.Exec(*opt.db)
-				if err != nil {
-					fmt.Println(err, row)
-					// fmt.Println(1)
-				}
-			}
-			bar.Add(1)
 		}
-		bar.Finish()
-
-		if *opt.useTransaction {
-			log.Print("Commit Start")
-			tx.Commit()
-			log.Print("Commit Finished")
-		}
-
-		return
+		bar.Add(1)
 	}
-*/
+	bar.Finish()
+
+	if *opt.useTransaction {
+		log.Print("Commit Start")
+		tx.Commit()
+		log.Print("Commit Finished")
+	}
+
+	return
+}
+
+func updateWithJet(data []model.User) {
+	if *opt.updateCount == 0 {
+		return
+	} 
+	log.Println("Executing updateWithJet")
+
+	var tx *sql.Tx
+	if *opt.useTransaction {
+		// Get a Tx for making transaction requests.
+		var err error
+		tx, err = opt.db.Begin()
+		if err != nil {
+			_, filename, line, _ := runtime.Caller(1)
+			log.Fatalf("[error] %s:%d %v", filename, line, err)
+		}
+		// Defer a rollback in case anything fails.
+		defer tx.Rollback()
+	}
+
+	updateCount := 0
+	bar := progressbar.Default(int64(*opt.updateCount)) //(len(data))) 
+	for _, rec := range data { 
+		updateCount++
+		if updateCount >= *opt.updateCount {
+			break
+		}
+		*rec.YearBirth-- 
+
+		columnList := ColumnList{
+			User.User, User.City, User.Region, User.Country, User.AreaCode, User.ZipCode,
+			User.YearBirth, User.Im, User.Name,
+		}
+
+		stmtInserUser := User.INSERT(columnList).
+			MODEL(rec).
+			ON_CONFLICT(User.User).
+			DO_UPDATE(
+				SET(
+					User.City.SET(User.EXCLUDED.City),
+					User.Country.SET(User.EXCLUDED.Country),
+					User.AreaCode.SET(User.EXCLUDED.AreaCode),
+					User.ZipCode.SET(User.EXCLUDED.ZipCode),
+					User.YearBirth.SET(User.EXCLUDED.YearBirth),
+					User.Im.SET(User.EXCLUDED.Im),
+					User.Name.SET(User.EXCLUDED.Name),
+				).WHERE(
+					OR(User.Country.IS_DISTINCT_FROM(User.EXCLUDED.Country)).
+						OR(User.AreaCode.IS_DISTINCT_FROM(User.EXCLUDED.AreaCode)).
+						OR(User.ZipCode.IS_DISTINCT_FROM(User.EXCLUDED.ZipCode)).
+						OR(User.YearBirth.IS_DISTINCT_FROM(User.EXCLUDED.YearBirth)).
+						OR(User.Im.IS_DISTINCT_FROM(User.EXCLUDED.Im)).
+						OR(User.Name.IS_DISTINCT_FROM(User.EXCLUDED.Name)),
+				),
+			)
+
+		// sql_debug := stmtInserUser.DebugSql()
+		// fmt.Println(sql_debug)
+		if *opt.useTransaction {
+			_, err := stmtInserUser.Exec(tx)
+			if err != nil {
+				_, filename, line, _ := runtime.Caller(1)
+				log.Fatalf("[error] %s:%d %v", filename, line, err)
+			}
+		} else {
+			_, err := stmtInserUser.Exec(opt.db)
+			if err != nil {
+				_, filename, line, _ := runtime.Caller(1)
+				log.Fatalf("[error] %s:%d %v", filename, line, err)
+			}
+		}
+		bar.Add(1)
+	}
+	bar.Finish()
+
+	if *opt.useTransaction {
+		log.Print("Commit Start")
+		tx.Commit()
+		log.Print("Commit Finished")
+	}
+
+	return
+}
+
+func selectWithJet(data []model.User) {
+	log.Println("Executing selectWithJet")
+
+	var tx *sql.Tx
+	if *opt.useTransaction {
+		// Get a Tx for making transaction requests.
+		var err error
+		tx, err = opt.db.Begin()
+		if err != nil {
+			_, filename, line, _ := runtime.Caller(1)
+			log.Fatalf("[error] %s:%d %v", filename, line, err)
+		}
+		// Defer a rollback in case anything fails.
+		defer tx.Rollback()
+	}
+
+	bar := progressbar.Default(int64(len(data)))
+	for _, rec := range data {
+
+		columnList := ColumnList{
+			User.User, User.City, User.Region, User.Country, User.AreaCode, User.ZipCode,
+			User.YearBirth, User.Im, User.Name, User.CreatedTst, User.ChangedTst,
+		}
+
+		stmtInserUser := User.SELECT(columnList).
+			FROM(User).
+			WHERE(User.User.EQ(String(rec.User)))
+
+		// sql_debug := stmtInserUser.DebugSql()
+		// fmt.Println(sql_debug)
+		if *opt.useTransaction {
+			_, err := stmtInserUser.Exec(tx)
+			if err != nil {
+				_, filename, line, _ := runtime.Caller(1)
+				log.Fatalf("[error] %s:%d %v", filename, line, err)
+			}
+		} else {
+			_, err := stmtInserUser.Exec(opt.db)
+			if err != nil {
+				_, filename, line, _ := runtime.Caller(1)
+				log.Fatalf("[error] %s:%d %v", filename, line, err)
+			}
+		}
+		bar.Add(1)
+	}
+	bar.Finish()
+
+	if *opt.useTransaction {
+		log.Print("Commit Start")
+		tx.Commit()
+		log.Print("Commit Finished")
+	}
+}
+
 func main() {
 	log.Println("Execution Starting")
 
 	cpuprofile := flag.String("cpuprofile", "", "write cpu profile to file")
 	opt.flipYearBirth = flag.Bool("flipYearBirth", false, "Flip YearBirth generation for Upsert Testing")
-	opt.rowCount = flag.Int("rowCount", 11000, "Number of rows to use in test")
+	opt.rowCount = flag.Int("rowCount", 10000, "Number of rows to use in test")
 	opt.updateCount = flag.Int("updateCount", 1000, "Maximum number of updates to perform")
+	opt.useBoth = flag.Bool("useBoth", false, "Run both RawSql and Jet")
 	opt.useJet = flag.Bool("useJet", false, "Run using Jet module")
+	opt.useRawSQL = flag.Bool("useRawSQL", false, "Run using RawSQL module")
 	opt.useTransaction = flag.Bool("useTransaction", false, "Wrap work in transaction")
 
 	flag.Parse()
+
+	if *opt.useBoth {
+		*opt.useRawSQL = true
+		*opt.useJet = true
+	} else if *opt.useJet {
+		*opt.useRawSQL = false
+	} else {
+		*opt.useRawSQL = true
+	}
 
 	if *cpuprofile != "" {
 		f, err := os.Create(*cpuprofile)
@@ -524,13 +616,25 @@ func main() {
 	})
 	log.Println("Sort data Ended")
 
-	//if *opt.useJet {
-	//	withJet(data, opt)
-	//} else {
-	insertWithRawSQLUpsert(data)
-	updateWithRawSQLUpsert(data)
-	selectWithRawSQLUpsert(data)
-	//}
+	if *opt.useRawSQL {
+		insertWithRawSQLUpsert(data)
+		updateWithRawSQLUpsert(data)
+		selectWithRawSQLUpsert(data)
+	}
+
+	if *opt.useJet {
+		if *opt.useBoth {
+			err = dbCleanUp()
+			if err != nil {
+				_, filename, line, _ := runtime.Caller(1)
+				log.Fatalf("[error] %s:%d %v", filename, line, err)
+			}
+			log.Print("Reset database for Jet")
+		}
+		insertWithJet(data)
+		updateWithJet(data)
+		selectWithJet(data)
+	}
 
 	log.Println("Exection Completed")
 }
